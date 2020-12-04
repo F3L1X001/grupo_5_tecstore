a2bacb2bfff945044b9baf3c9c0b2bf5f77aee4f
GonzaloArias <gonza_95lp@hotmail.com>
2020-12-04 16:23:26 -0300

    carga de productos

--- site/controllers/index_controller.js
+++ site/controllers/index_controller.js
@@ -1,6 +1,7 @@
 const path = require('path'); 
 const fs = require('fs');
 const { json } = require('express');
+const { search } = require('../routes');
 
 function todo_los_productos() {
     const productos_ubicacion_BD = path.join(__dirname, '../data', 'datos-productos.json');
@@ -11,13 +12,23 @@ function todo_los_productos() {
 
 const indexController = {
     home: function (req, res){
-        const productos = todo_los_productos();
-        
-        res.render('index',{
-            productos: productos
+
+        const destacProduct = todo_los_productos().filter ((product) => {
+            return product.category == 'destacados';
         });
+
+        const ultProduct = todo_los_productos().filter ((product) => {
+            return product.category == 'ultimos';
+        });
+
+        res.render('index',{
+            productosDestacados:destacProduct,
+            ultimosProductos: ultProduct
+            
+        });
+
     }
-  
+   
 };
 
 module.exports = indexController;
