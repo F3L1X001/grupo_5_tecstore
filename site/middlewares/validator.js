8ea5b8ac72ab477c0f5ff355ffe4514926165071
lixxx01 <oktubre0282@gmail.com>
2021-01-22 21:02:04 -0300

    Validaciones Users BackEnd realizadas.

--- site/middlewares/validator.js
+++ site/middlewares/validator.js
@@ -2,12 +2,17 @@ const express = require('express');
 const path = require('path');
 const { check, validationResult, body } = require('express-validator');
 const fs = require('fs');
+const db = require('../database/models');
+
 
 module.exports = {
     register:[
         body('nombre')
             .notEmpty()
-                .withMessage('El nombre no puede estar vacio'),
+                .withMessage('El nombre no puede estar vacio')
+                .bail()
+            .isLength({min:2})
+                .withMessage('El nombre debe tener minimo 2 caracteres'), 
         body('email')
             .notEmpty()
                 .withMessage('El email no puede estar vacio')
@@ -17,25 +22,29 @@ module.exports = {
                 .bail()
             .custom(function(value){
                 
-                const usersReadDB = path.join(__dirname, '../data', 'datos-usuarios.json');
-                const users = JSON.parse(fs.readFileSync(usersReadDB, 'utf-8'));
+                return db.User.findOne({
+                        where: {
+                            email: value
+                        }
+                }).then (user =>{
+                    if(user){
+                        return Promise.reject('El email ya existe')
+                    }
+                })}),
                 
-                const emailFound = users.find(function (user){return user.email == value});
-
-                return !emailFound;
-
-            })
-                .withMessage('El correo ya existe'),
         body('password')
-            .isLength({min:6})
-                .withMessage('La contraseña debe tener minimo 6 caracteres.')
+            .isLength({min:8})
+                .withMessage('La contraseña debe tener minimo 8 caracteres.') 
                 .bail()
             .custom(function(value, { req }){
                 if(value == req.body.password1){
                     return true;
                 } else { return false;}
                 })
-                .withMessage('Las passwords tiene que ser iguales'),
+                .withMessage('Las passwords tiene que ser iguales')
+                .bail()
+            .isStrongPassword({minLength: 8, minLowercase: 1, minUppercase: 1, minNumbers: 1, minSymbols: 1})
+                .withMessage('La contraseña debe tener min 8 caracteres, 1 Maysc, 1 Minsc, 1 numero y 1 caracter especial'),
         body('avatar')
             .custom(function (value, { req }){
                 return req.files[0];
@@ -44,12 +53,12 @@ module.exports = {
                 .bail()
             .custom(function(value, { req }){
             const ext = path.extname(req.files[0].originalname);
-            if( ext == '.jpg' || ext == '.png' || ext == '.jpeg'){
+            if( ext == '.jpg' || ext == '.png' || ext == '.jpeg' || ext == '.gif'){
                 return true;
             }
                 return false;
             })
-                .withMessage('El archivo de imagen debe ser .jpg/.png/.jpeg')
+                .withMessage('El archivo de imagen debe ser .jpg/.png/.jpeg/.gif')
     ],
     login: [
         body('email')
@@ -60,6 +69,16 @@ module.exports = {
             .withMessage('Debe tener formato Email'),
         body('password')
         .notEmpty()
-            .withMessage('La contraseña no puede estar vacia')
+            .withMessage('La contraseña no puede estar vacia') // validado y existir en la BD
+    ],
+    createProduct: [
+        /* Validar nombre obligatorio, minimo 5 caracteres.
+        Descripcion validar con min 20 caracteres.
+        Imagen validar con extensiones.*/
+    ],
+    editProduct: [
+         /* Validar nombre obligatorio, minimo 5 caracteres.
+        Descripcion validar con min 20 caracteres.
+        Imagen validar con extensiones.*/
     ]    
 }
