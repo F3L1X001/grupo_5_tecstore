6e986ee4d20c5691adbcd46a49a932a1ba50930e
lixxx01 <oktubre0282@gmail.com>
2021-02-13 17:54:33 -0300

    Api list, falta URL

--- site/controllers/api_controller.js
+++ site/controllers/api_controller.js
@@ -7,12 +7,13 @@ module.exports={
        
        const {email}=req.query;
        console.log('soy el LOG '+ email);
-         const user =await User.findOne({
-              where: {
+         const user =await User.findAll({
+             attributes:['email', 'id', 'name']
+/*               where: {
 
                      email:email
 
-              }
+              } */
          })
          res.json({
              meta:{
