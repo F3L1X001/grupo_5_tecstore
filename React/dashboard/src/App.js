834d47cd4cb02f6962ea73d04584741e94a3e54f
leonardo <lgramaldo@gmail.com>
2021-02-21 09:07:52 -0300

    se crea React, se crea modulo practica ApiGif

---
+++ React/dashboard/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import ApiGif from './components/practica/ApiGif'
+function App() {
+  
+
+
+  return (
+    <div className="App">
+      
+       <ApiGif/>
+    </div>
+  );
+}
+
+export default App;
